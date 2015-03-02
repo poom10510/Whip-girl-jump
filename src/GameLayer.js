d9e4729ea54ca3d0a3b85ae2497012e00e2ee960
kitipoom <kitipoom@gmail.com>
2015-03-02 21:28:17 +0700

    Girl move left-right

--- src/GameLayer.js
+++ src/GameLayer.js
@@ -2,6 +2,7 @@ var GameLayer = cc.LayerColor.extend({
     init: function() {
         this._super( new cc.Color( 127, 127, 127, 255 ) );
         this.setPosition( new cc.Point( 0, 0 ) );
+        this.addKeyboardHandlers();
         //
         this.player = new Girl();
         this.player.setPosition( new cc.Point( screenWidth / 2, screenHeight / 2 ) );
@@ -12,6 +13,29 @@ var GameLayer = cc.LayerColor.extend({
         this.state = GameLayer.STATES.FRONT;
  
         return true;
+    },
+    onKeyUp: function( e ) {
+       console.log( 'Up: ' + e );
+    },
+    onKeyDown: function( e ) {
+    if ( e == cc.KEY.right) {
+        this.player.switchDirection1();
+    }
+    else if( e == cc.KEY.left){
+    this.player.switchDirection();
+    }
+    },
+    addKeyboardHandlers: function() {
+        var self = this;
+        cc.eventManager.addListener({
+            event: cc.EventListener.KEYBOARD,
+            onKeyPressed : function( e ) {
+                self.onKeyDown( e );
+            },
+            onKeyReleased: function( e ) {
+                self.onKeyUp( e );
+            }
+        }, this);
     }
 });
  GameLayer.STATES = {
--- src/Girl.js
+++ src/Girl.js
@@ -3,11 +3,11 @@ var Girl = cc.Sprite.extend({
         this._super();
         this.initWithFile( 'res/images/Whip_Girl.png' );
        	this.direction = Girl.DIR.RIGHT;
-        this.speed=5;
+        this.speed=10;
     },
     update: function( dt ) {
 	var pos = this.getPosition();
-       if(this.direction == 1 ) {
+     /*  if(this.direction == 1 ) {
 	   if ( pos.y < screenHeight ) {
            this.setPosition( new cc.Point( pos.x, pos.y + this.speed ) );
 	   } else {
@@ -31,8 +31,13 @@ var Girl = cc.Sprite.extend({
 	   } else {
            this.setPosition( new cc.Point( screenWidth, pos.y ) );
        }
+       }*/
+        if ( pos.x > screenWidth ) {
+           this.setPosition( new cc.Point( 0, pos.y ) );
        }
-          
+        else if ( pos.x < 0 ) {
+           this.setPosition( new cc.Point( screenWidth, pos.y ) );
+     } 
     },
     
    switchDirection: function() {
@@ -41,6 +46,8 @@ var Girl = cc.Sprite.extend({
 	    this.direction = Girl.DIR.LEFT;
 	   
 	}
+  var pos = this.getPosition();
+  this.setPosition( new cc.Point( pos.x-this.speed, pos.y ) );
   /*else if( this.direction == Girl.DIR.LEFT ) {
 	    this.direction = Girl.DIR.RIGHT;
 	    
@@ -54,7 +61,8 @@ var Girl = cc.Sprite.extend({
       this.direction = Girl.DIR.RIGHT;
       
   }
-       
+      var pos = this.getPosition(); 
+       this.setPosition( new cc.Point( pos.x+this.speed, pos.y ) );
     },
      closeTo: function( obj ) {
 	var myPos = this.getPosition();
